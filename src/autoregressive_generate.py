fd48ce61731de601661feb1f3848933947f5905f
utkarsh4430 <utkarsh4430@gmail.com>
2024-06-03 05:06:21 -0700

    Updated code

--- src/autoregressive_generate.py
+++ src/autoregressive_generate.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from transformers import pipeline, AutoTokenizer
 from tqdm import tqdm
-filepath = '/fs/nexus-projects/audio-visual_dereverberation/sonal/diff/data/low_res/100/classification/huff_train_processed_filtered_generated.source'
-tokenizer = AutoTokenizer.from_pretrained('/fs/nexus-projects/audio-visual_dereverberation/sonal/amr-data-augmentation/checkpoint-7480')
-model_pipeline = pipeline("text2text-generation", model='/fs/nexus-projects/audio-visual_dereverberation/sonal/amr-data-augmentation/checkpoint-7480', tokenizer=tokenizer, device=0)
+filepath = './low_res/100/classification/huff_train_processed_filtered_generated.source'
+tokenizer = AutoTokenizer.from_pretrained('./amr-data-augmentation/checkpoint-7480')
+model_pipeline = pipeline("text2text-generation", model='./amr-data-augmentation/checkpoint-7480', tokenizer=tokenizer, device=0)
 
 with open(filepath, 'r') as f:
     text = f.readlines()
 
 text = [x.strip() for x in text]
 
-train_og = pd.read_csv('/fs/nexus-projects/audio-visual_dereverberation/sonal/diff/data/low_res/100/classification/huff_train.tsv', delimiter='\t')
+train_og = pd.read_csv('./low_res/100/classification/huff_train.tsv', delimiter='\t')
 train_og = train_og.values
 
 assert len(train_og) == len(text)
